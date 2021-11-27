d88f1e696042ee601ec209c85c4a15ac3a1f9b73
Orion042 <s1921062@cco.kanagawa-it.ac.jp>
2021-11-28 04:05:17 +0900

    関数として切り分け作業の開始．途中まで完了しました．

--- peg.py
+++ peg.py
@@ -4,3 +4,33 @@ import math
 import sys
 import os, os.path
 
+def startConvert(startNumber: int,endNumber: int,beforeExtension: str,afterExtension: str) -> bool:
+    
+    for i in range(startNumber,endNumber+1):
+            #countConvert=str(i)
+            countConvert = str(i).zfill(p)
+            print(countConvert)
+            (
+                ffmpeg
+                .input('{}.' + beforeExtension.format(countConvert))
+                .output('{}.' + afterExtension.format(countConvert))
+                .run()
+            )
+
+    finalNum = len([name for name in os.listdir(".") if os.path.isfile(name)])
+
+    if (countAll * 2 == finalNum):
+        return True
+    else: 
+        return False
+
+def mainProgram():
+
+    countAll = len([name for name in os.listdir(".") if os.path.isfile(name)])
+    p=int(math.log10(countAll)+1)
+
+    startConvert(0,10,"webp","jpg")
+
+
+if __name__ == '__main__':
+    mainProgram()
